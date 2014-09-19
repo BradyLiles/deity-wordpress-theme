aa0c274dea456648ca3c0d29ee8d6bc0ca11274e
Brady Liles <bradyliles@outlook.com>
2014-09-18 21:15:04 -0600

    Added comment for testing purposes.

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 		<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'Deity Web Design' ), '<a href="http://deitywebdesign.com/">Deity Web Design</a>' ); ?>
 	</div>
 </footer>
-</div>
+</div>//Must include wp_footer hook for pluginss
 <?php wp_footer(); ?>
 </body>
 </html>
